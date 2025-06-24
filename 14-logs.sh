1411746d4972afaf7cc07865a28255cab482b73f
Devaraj <devarajthota@gmail.com>
2025-06-24 17:06:15 +0530

    colors

--- 14-logs.sh
+++ 14-logs.sh
@@ -5,14 +5,12 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
-
 LOGS_FOLDER="/var/log/shellscript-logs"    
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER   
 echo "script started executing at: $(date)" | tee -a $LOG_FILE
-
 
 if [ $USERID -ne 0 ]
 then
@@ -30,9 +28,9 @@ VALIDATE(){
     else 
         echo -e "Installing $2 is ... $R FAILURE $N" | tee -a $LOG_FILE
         exit 1
-    fi
-    
+    fi   
 }
+
 dnf list installed mysql &>>$LOG_FILE
 
 if [ $? -ne 0 ]
@@ -55,7 +53,6 @@ else
 fi
 
 dnf list installed nginx &>>$LOG_FILE
-
 if [ $? -ne 0 ]
 then
     echo "nginx is not installed... going to install it" | tee -a $LOG_FILE
