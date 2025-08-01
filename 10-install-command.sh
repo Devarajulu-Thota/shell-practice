82f1926d79d2bd435b6a368440c3040f598b7bf1
Devaraj <devarajthota@gmail.com>
2025-08-01 15:15:28 +0530

    06-datatypes.sh

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -5,35 +5,35 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
-    exit 1 #give other than 0 upto 127
+    # exit 1 #give other than 0 upto 127
 else
     echo "You are running with root access"
 fi 
 
-dnf list installed mysql
+# dnf list installed mysql
 
-# check already installed or not. if Installed $? is 0, then
-# If not installed $? is not 0. expression is true 
-if [ $? -ne 0 ]
-then 
-    echo "MYSQL is not installed... going to install it"
-    dnf install mysql -y
-    if [ $? -eq 0 ]
-    then
-        echo "Installing MYSQL is ... SUCCESS"
-    else 
-        echo "Installing MYSQL is ... FAILURE" 
-        exit 1
-    fi
-else
-    echo "MYSQL is already installed...Nothing to do"
-fi
-# dnf install mysql -y
-
-# if [ $? -eq 0 ]
-# then
-#     echo "Installing MYSQL is ... SUCCESS"
-# else 
-#     echo "Installing MYSQL is ... FAILURE" 
-#     exit 1
+# # check already installed or not. if Installed $? is 0, then
+# # If not installed $? is not 0. expression is true 
+# if [ $? -ne 0 ]
+# then 
+#     echo "MYSQL is not installed... going to install it"
+#     dnf install mysql -y
+#     if [ $? -eq 0 ]
+#     then
+#         echo "Installing MYSQL is ... SUCCESS"
+#     else 
+#         echo "Installing MYSQL is ... FAILURE" 
+#         exit 1
+#     fi
+# else
+#     echo "MYSQL is already installed...Nothing to do"
 # fi
+# # dnf install mysql -y
+
+# # if [ $? -eq 0 ]
+# # then
+# #     echo "Installing MYSQL is ... SUCCESS"
+# # else 
+# #     echo "Installing MYSQL is ... FAILURE" 
+# #     exit 1
+# # fi
