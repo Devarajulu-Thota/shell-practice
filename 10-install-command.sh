d9db09f3c26fce3a76aacba945e11b54c6c7258c
Devaraj <devarajthota@gmail.com>
2025-08-01 15:32:20 +0530

    06-datatypes.sh

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -10,7 +10,7 @@ else
     echo "You are running with root access"
 fi 
 
-dnf install mysqldfddfd -y
+dnf install mysql -y
 # dnf list installed mysql
 
 # # check already installed or not. if Installed $? is 0, then
