3d76ed0b5eb88bfbdd1546449918cc4c0a184148
Devaraj <devarajthota@gmail.com>
2025-08-19 16:02:34 +0530

    special vars

--- 10-install-command.sh
+++ 10-install-command.sh
@@ -6,10 +6,14 @@ if [ $USERID -ne 0 ]
 then
     echo "ERROR:: Please run this script with root access"
     exit 1 #give other than 0 upto 127
+else
+    echo "You are running with root access"
 fi
 
 dnf list installed mysql
 
+# check already installed or not. if installed $? is 0, then 
+# If not installed $? is not 0. expression is true
 if [ $? -ne 0 ]
 then
     echo "MYSQL is not installed... going to install it"
